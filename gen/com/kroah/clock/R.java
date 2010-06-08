5377941adb9753d25eeb447cf51d0c22feeee6c5
Greg Kroah-Hartman <gregkh@suse.de>
2010-06-08 14:58:34 -0700

    added menu icons

--- gen/com/kroah/clock/R.java
+++ gen/com/kroah/clock/R.java
@@ -12,9 +12,21 @@ public final class R {
     }
     public static final class drawable {
         public static final int icon=0x7f020000;
+        public static final int menu_about=0x7f020001;
+        public static final int menu_add=0x7f020002;
+        public static final int menu_close=0x7f020003;
+        public static final int menu_preferences=0x7f020004;
+    }
+    public static final class id {
+        public static final int delete_contact=0x7f060001;
+        public static final int edit_contact=0x7f060000;
+        public static final int exit=0x7f060002;
     }
     public static final class layout {
         public static final int main=0x7f030000;
+    }
+    public static final class menu {
+        public static final int menu=0x7f050000;
     }
     public static final class string {
         public static final int app_name=0x7f040001;
--- src/com/kroah/clock/Clock.java
+++ src/com/kroah/clock/Clock.java
@@ -2,8 +2,44 @@ package com.kroah.clock;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
 
 public class Clock extends Activity {
+	
+	static final private int MENU_ADD = 0;
+	static final private int MENU_SETTINGS = 1;
+	static final private int MENU_ABOUT = 2;
+	static final private int MENU_EXIT = 3;
+	
+	/* Creates the menu items */
+	public boolean onCreateOptionsMenu(Menu menu) {
+		menu.add(0, MENU_ADD, 0, "Add").setIcon(R.drawable.menu_add);
+	    menu.add(0, MENU_SETTINGS, 0, "Settings").setIcon(R.drawable.menu_preferences);
+	    menu.add(0, MENU_ABOUT, 0, "About").setIcon(R.drawable.menu_about);
+	    menu.add(0, MENU_EXIT, 0, "Exit").setIcon(R.drawable.menu_close);
+	    return true;
+	}
+
+	/* Handles item selections */
+	public boolean onOptionsItemSelected(MenuItem item) {
+	    switch (item.getItemId()) {
+	    case MENU_ADD:
+	        //newGame();
+	        return true;
+	    case MENU_SETTINGS:
+	        //quit();
+	        return true;
+	    case MENU_ABOUT:
+	    	return true;
+	    case MENU_EXIT:
+	    	finish();
+	    	return true;
+	    }
+	    return false;
+	}
+	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
